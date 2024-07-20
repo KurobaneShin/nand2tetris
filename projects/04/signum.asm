6afd6c91fdd85b3e5ac613977a3d3f61ff02b75d
kurobane <darkflamemaster663@gmail.com>
2024-07-19 23:45:37 -0300

    make some exercices to refresh my memory

---
+++ projects/04/signum.asm
@@ -0,0 +1,18 @@
+@R0
+D=M
+
+@POSITIVE
+D;JGT //if D > 0 (BECAUSE R0 HAS VALUE OF 0)
+
+@R1
+M=0
+@END
+0;JMP
+
+(POSITIVE)
+@R1
+M=1
+
+(END)
+@END
+0;JMP
---
+++ projects/04/sum.asm
@@ -0,0 +1,12 @@
+@R0
+D=M //D = RAM[0]
+
+@R1
+D=D+M // D = D + RAM[1]
+
+@R2
+M=D //RAM[2] = D
+
+@R6
+0;JMP 
+//end program with infinite loop
